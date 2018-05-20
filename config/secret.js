df2ba6392beda43dfecc49ca6f373a4484e73fd0
Luis Gustavo <gustavoluisrh@gmail.com>
2018-05-20 12:59:07 -0300

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-  database: process.env.DATABASE || 'mongodb://sparta:mudar123@ds121950.mlab.com:21950/fiver',
+  database: process.env.DATABASE || '',
   port: process.env.PORT || 3000,
-  secret: process.env.SECRET || 'SpartaDev2222',
+  secret: process.env.SECRET || '',
 
 }
